4c5e56a1804ebbd82cbee04fff821ae82195bc71
masoud <roohbakhsh.masoud@gmail.com>
2015-10-07 16:54:58 +0330

    add department manager interfaces for manage department and its fields

---
+++ Interfaces/iDepartmentManager.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: masoud
+ * Date: 10/7/15
+ * Time: 4:51 PM
+ */
+
+namespace Ticket\Interfaces;
+
+
+interface iDepartmentManager
+{
+    function getDepartments();
+    function getDepartmentFields();
+}
